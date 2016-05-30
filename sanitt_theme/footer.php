54aaa290d8a3198150a3fbbc77535bf2d79819ff
Deltachi <dominik.scheffler@me.com>
2016-05-30 13:38:15 +0200

    Remove everything
    Step 1 for a clean update

--- sanitt_theme/footer.php
+++
@@ -1,7 +0,0 @@
-	<fixed-navbar active-tab="<?php echo the_field('active_page'); ?>"></fixed-navbar>		
-	<section class="footer">
-		<my-footer></my-footer>
-	</section>
-	<?php wp_footer(); //Crucial footer hook! ?>
-	</body>
-</html>
